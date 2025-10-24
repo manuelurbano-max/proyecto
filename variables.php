1716f80640879ceb231a8cc52d2ac3615163269c
manuelurbano-max <manuel.urbano@cun.edu.co>
2025-10-23 20:26:19 -0500

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php 
+// Variables de diferentes tipos
+$nombre = "Juan";
+// String (texto)
+$edad = 25;
+// Integer (número entero)
+$altura = 1.75;
+// Float (decimal)
+$esEstudiante = true;
+
+// Boolean (verdadero/falso) // Mostrar las variables
+echo "Nombre: " . $nombre; 
+echo "<br>Edad: " . $edad; 
+echo "<br>Altura: " . $altura . " metros";
+ 
+ ?>
